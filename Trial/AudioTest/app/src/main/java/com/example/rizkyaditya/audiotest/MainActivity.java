7b57f540ce9634d722dd9e38fb80c953d6a0527d
rizkyadit05 <rizkyadit05@gmail.com>
2016-04-02 18:23:25 +0700

    Percobaan
    Audio as Background service

---
+++ Trial/AudioTest/app/src/main/java/com/example/rizkyaditya/audiotest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.rizkyaditya.audiotest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Method to start the service
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    // Method to stop the service
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
+    }
+}
