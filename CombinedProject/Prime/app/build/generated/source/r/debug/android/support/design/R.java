c2e3d9a3887de7d9fb7858db5d5d0db3f773edd8
raliyarmdn <raliyarmdn@gmail.com>
2016-04-09 21:21:39 +0700

    Merubah tampilan bluetooth activity
    - nambahin asset custom scroll xml di drawable
    - ngerubah background dll sesuai dengan skema warna yg ada

--- CombinedProject/Prime/app/build/generated/source/r/debug/android/support/design/R.java
+++ CombinedProject/Prime/app/build/generated/source/r/debug/android/support/design/R.java
@@ -564,9 +564,9 @@ public final class R {
 		public static final int abc_textfield_search_activated_mtrl_alpha = 0x7f020042;
 		public static final int abc_textfield_search_default_mtrl_alpha = 0x7f020043;
 		public static final int abc_textfield_search_material = 0x7f020044;
-		public static final int design_fab_background = 0x7f020045;
-		public static final int design_snackbar_background = 0x7f020046;
-		public static final int notification_template_icon_bg = 0x7f02004b;
+		public static final int design_fab_background = 0x7f020046;
+		public static final int design_snackbar_background = 0x7f020047;
+		public static final int notification_template_icon_bg = 0x7f02004c;
 	}
 	public static final class id {
 		public static final int action0 = 0x7f0c007c;
