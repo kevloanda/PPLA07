0920ae6f0ced55aaff1d5a256c189051192f4b53
christianh13 <christian_halim_13@yahoo.co.id>
2016-05-04 22:35:04 +0700

    Tambah menu burger

--- CombinedProject/Prime/app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
+++ CombinedProject/Prime/app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
@@ -385,9 +385,9 @@ public final class R {
 		public static final int highlight_alpha_material_colored = 0x7f090064;
 		public static final int highlight_alpha_material_dark = 0x7f090065;
 		public static final int highlight_alpha_material_light = 0x7f090066;
-		public static final int notification_large_icon_height = 0x7f09006a;
-		public static final int notification_large_icon_width = 0x7f09006b;
-		public static final int notification_subtext_size = 0x7f09006c;
+		public static final int notification_large_icon_height = 0x7f09006b;
+		public static final int notification_large_icon_width = 0x7f09006c;
+		public static final int notification_subtext_size = 0x7f09006d;
 	}
 	public static final class drawable {
 		public static final int abc_ab_share_pack_mtrl_alpha = 0x7f020000;
@@ -465,10 +465,10 @@ public final class R {
 		public static final int abc_textfield_search_activated_mtrl_alpha = 0x7f020048;
 		public static final int abc_textfield_search_default_mtrl_alpha = 0x7f020049;
 		public static final int abc_textfield_search_material = 0x7f02004a;
-		public static final int notification_template_icon_bg = 0x7f020056;
+		public static final int notification_template_icon_bg = 0x7f020058;
 	}
 	public static final class id {
-		public static final int action0 = 0x7f0e008d;
+		public static final int action0 = 0x7f0e0092;
 		public static final int action_bar = 0x7f0e005a;
 		public static final int action_bar_activity_content = 0x7f0e0000;
 		public static final int action_bar_container = 0x7f0e0059;
@@ -477,7 +477,7 @@ public final class R {
 		public static final int action_bar_subtitle = 0x7f0e003b;
 		public static final int action_bar_title = 0x7f0e003a;
 		public static final int action_context_bar = 0x7f0e005b;
-		public static final int action_divider = 0x7f0e0091;
+		public static final int action_divider = 0x7f0e0096;
 		public static final int action_menu_divider = 0x7f0e0002;
 		public static final int action_menu_presenter = 0x7f0e0003;
 		public static final int action_mode_bar = 0x7f0e0057;
@@ -488,9 +488,9 @@ public final class R {
 		public static final int always = 0x7f0e0033;
 		public static final int beginning = 0x7f0e0031;
 		public static final int buttonPanel = 0x7f0e0044;
-		public static final int cancel_action = 0x7f0e008e;
+		public static final int cancel_action = 0x7f0e0093;
 		public static final int checkbox = 0x7f0e0052;
-		public static final int chronometer = 0x7f0e0094;
+		public static final int chronometer = 0x7f0e0099;
 		public static final int collapseActionView = 0x7f0e0034;
 		public static final int contentPanel = 0x7f0e004a;
 		public static final int custom = 0x7f0e0050;
@@ -500,7 +500,7 @@ public final class R {
 		public static final int disableHome = 0x7f0e000e;
 		public static final int edit_query = 0x7f0e005c;
 		public static final int end = 0x7f0e0021;
-		public static final int end_padder = 0x7f0e0099;
+		public static final int end_padder = 0x7f0e009e;
 		public static final int expand_activities_button = 0x7f0e003e;
 		public static final int expanded_menu = 0x7f0e0051;
 		public static final int home = 0x7f0e0004;
@@ -508,12 +508,12 @@ public final class R {
 		public static final int icon = 0x7f0e0042;
 		public static final int ifRoom = 0x7f0e0035;
 		public static final int image = 0x7f0e003f;
-		public static final int info = 0x7f0e0098;
-		public static final int line1 = 0x7f0e0092;
-		public static final int line3 = 0x7f0e0096;
+		public static final int info = 0x7f0e009d;
+		public static final int line1 = 0x7f0e0097;
+		public static final int line3 = 0x7f0e009b;
 		public static final int listMode = 0x7f0e000b;
 		public static final int list_item = 0x7f0e0041;
-		public static final int media_actions = 0x7f0e0090;
+		public static final int media_actions = 0x7f0e0095;
 		public static final int middle = 0x7f0e0032;
 		public static final int multiply = 0x7f0e0027;
 		public static final int never = 0x7f0e0036;
@@ -547,13 +547,13 @@ public final class R {
 		public static final int src_atop = 0x7f0e0029;
 		public static final int src_in = 0x7f0e002a;
 		public static final int src_over = 0x7f0e002b;
-		public static final int status_bar_latest_event_content = 0x7f0e008f;
+		public static final int status_bar_latest_event_content = 0x7f0e0094;
 		public static final int submit_area = 0x7f0e0065;
 		public static final int tabMode = 0x7f0e000d;
-		public static final int text = 0x7f0e0097;
-		public static final int text2 = 0x7f0e0095;
+		public static final int text = 0x7f0e009c;
+		public static final int text2 = 0x7f0e009a;
 		public static final int textSpacerNoButtons = 0x7f0e004d;
-		public static final int time = 0x7f0e0093;
+		public static final int time = 0x7f0e0098;
 		public static final int title = 0x7f0e0043;
 		public static final int title_template = 0x7f0e0048;
 		public static final int topPanel = 0x7f0e0047;
@@ -595,18 +595,18 @@ public final class R {
 		public static final int abc_search_dropdown_item_icons_2line = 0x7f030016;
 		public static final int abc_search_view = 0x7f030017;
 		public static final int abc_select_dialog_material = 0x7f030018;
-		public static final int notification_media_action = 0x7f030030;
-		public static final int notification_media_cancel_action = 0x7f030031;
-		public static final int notification_template_big_media = 0x7f030032;
-		public static final int notification_template_big_media_narrow = 0x7f030033;
-		public static final int notification_template_lines = 0x7f030034;
-		public static final int notification_template_media = 0x7f030035;
-		public static final int notification_template_part_chronometer = 0x7f030036;
-		public static final int notification_template_part_time = 0x7f030037;
-		public static final int select_dialog_item_material = 0x7f030038;
-		public static final int select_dialog_multichoice_material = 0x7f030039;
-		public static final int select_dialog_singlechoice_material = 0x7f03003a;
-		public static final int support_simple_spinner_dropdown_item = 0x7f03003d;
+		public static final int notification_media_action = 0x7f030033;
+		public static final int notification_media_cancel_action = 0x7f030034;
+		public static final int notification_template_big_media = 0x7f030035;
+		public static final int notification_template_big_media_narrow = 0x7f030036;
+		public static final int notification_template_lines = 0x7f030037;
+		public static final int notification_template_media = 0x7f030038;
+		public static final int notification_template_part_chronometer = 0x7f030039;
+		public static final int notification_template_part_time = 0x7f03003a;
+		public static final int select_dialog_item_material = 0x7f03003b;
+		public static final int select_dialog_multichoice_material = 0x7f03003c;
+		public static final int select_dialog_singlechoice_material = 0x7f03003d;
+		public static final int support_simple_spinner_dropdown_item = 0x7f030040;
 	}
 	public static final class string {
 		public static final int abc_action_bar_home_description = 0x7f070000;
---
+++ CombinedProject/Prime/app/src/main/java/info/ppla07/prime/activity/HomeActivity.java
@@ -0,0 +1,196 @@
+package info.ppla07.prime.activity;
+
+import android.app.Activity;
+
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import info.ppla07.prime.R;
+import info.ppla07.prime.helper.SQLiteHandler;
+import info.ppla07.prime.helper.SessionManager;
+
+public class HomeActivity extends Activity
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
+    private TextView txtName;
+    private TextView txtEmail;
+    private Button btnEmergencyArea;
+    private ImageView btnBluetooth;
+    private Button btnContact;
+
+    private SQLiteHandler db;
+    private SessionManager session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        txtName = (TextView) findViewById(R.id.welcome);
+        btnEmergencyArea = (Button) findViewById(R.id.btnEmergencyArea);
+        btnBluetooth = (ImageView) findViewById(R.id.btnBluetooth);
+        btnContact = (Button) findViewById(R.id.btnContact);
+
+        startService(new Intent(this, BluetoothService.class));
+
+//		// SqLite database handler
+//		db = new SQLiteHandler(getApplicationContext());
+//
+//		// session manager
+//		session = new SessionManager(getApplicationContext());
+//
+//		if (!session.isLoggedIn()) {
+//			logoutUser();
+//		}
+
+//		// Fetching user details from SQLite
+//		HashMap<String, String> user = db.getUserDetails();
+
+//		String name = user.get("name");
+
+        String name = "PRIME";
+
+        // Displaying the user details on the screen
+        txtName.setText("Welcome, " + name);
+
+        // Logout button click event
+        btnEmergencyArea.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // Bluetooth button click event
+        btnBluetooth.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, BluetoothActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Contact button click event
+        btnContact.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+//				SharedPreferences sp = getSharedPreferences("MyPreference", Context.MODE_PRIVATE);
+//				SharedPreferences.Editor editor = sp.edit();
+//				editor.putString("EmergencyContactsNames","");
+//				editor.putString("EmergencyContactsNumbers","");
+//				editor.commit();
+//				Log.d("Error1", sp.getString("EmergencyContactsNames", ""));
+//				Log.d("Error1",sp.getString("EmergencyContactsNumbers",""));
+                Intent intent = new Intent(HomeActivity.this, ContactEmergency.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .commit();
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+        }
+    }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((HomeActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+
+    }
+
+}
